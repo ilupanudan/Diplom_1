aca076abcff077696967f28265dbbe05c5da375d
Ilia Mihalev <ilupanudan@yandex.ru>
2024-11-24 01:12:13 +0300

    readded tests

--- src/test/java/BunTest.java
+++ src/test/java/BunTest.java
@@ -1,21 +1,18 @@
 
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
-import org.mockito.junit.MockitoJUnitRunner;
 import praktikum.Bun;
 
 import static org.junit.Assert.assertEquals;
 
 @RunWith(Parameterized.class)
-public class TestBun {
-    private Bun bun;
+public class BunTest {
     private final String name;
     private final Float price;
 
-    public TestBun(String name, float price){
+    public BunTest(String name, float price){
         this.name = name;
         this.price = price;
 
@@ -31,22 +28,19 @@ public class TestBun {
         };
     }
 
-
     @Test
     public void getBunNameTest() {
         Bun bun = new Bun(name, price);
         String actual = bun.getName();
-        System.out.println("data: " + name);
-        System.out.println("actual: " + actual);
+
         assertEquals(name, actual);
 
     }
-    @Test
+   @Test
     public void getBunPriceTest() {
         Bun bun = new Bun(name, price);
         float actual = bun.getPrice();
-        System.out.println("data: " + price);
-        System.out.println("actual: " + actual);
+
         assertEquals(price, actual,0);
 
     }
